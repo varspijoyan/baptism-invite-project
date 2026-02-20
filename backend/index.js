95b0858e373fb470343b28cfc86c21c8ad37ba20
varspijoyan <varsikpijoyan2003@gmail.com>
2026-02-20 19:57:30 +0400

    Integrate route in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import mailRoute from "./route/mailRoute.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", mailRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
