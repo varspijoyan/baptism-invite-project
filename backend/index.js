4c76f65420dfebcff63b341dbb97396a95bcab8c
varspijoyan <varsikpijoyan2003@gmail.com>
2026-02-22 17:24:56 +0400

    Update cors

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,7 @@ import express from "express";
 import mailRoute from "./route/mailRoute.js";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5174",
-    methods: ["POST", "GET"],
-     allowedHeaders: ["Content-Type"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", mailRoute);
