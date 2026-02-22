170acb71f4a822ff104f4054a6c2bf63da3cb101
varspijoyan <varsikpijoyan2003@gmail.com>
2026-02-22 14:06:22 +0400

    Remove env variable

--- frontend/src/services/auth/api.ts
+++ frontend/src/services/auth/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = 'https://baptism-invite-8a1x.onrender.co';
 
 if (!BACKEND_URL) {
   throw new Error("VITE_BACKEND_URL is not defined");
