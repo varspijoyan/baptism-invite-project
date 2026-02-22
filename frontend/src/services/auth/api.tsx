5b366a7c7e66d0871c068dc2061f8b1788c7d202
varspijoyan <varsikpijoyan2003@gmail.com>
2026-02-22 13:37:45 +0400

    Fix api.tsx

--- frontend/src/services/auth/api.tsx
+++ frontend/src/services/auth/api.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.BACKEND_URL}/api`;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+if (!BACKEND_URL) {
+  throw new Error("VITE_BACKEND_URL is not defined");
+}
 
 const api = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: `${BACKEND_URL}/api`,
 });
 
 export { api };
