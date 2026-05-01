ec0033256b5ff230e27beda2e9cc562903047ac9
varspijoyan <varsikpijoyan2003@gmail.com>
2026-05-01 20:09:25 +0400

    Change main section images

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -1,5 +1,7 @@
 import crossImg from "../assets/images/Cross.svg";
-import image from "../assets/images/IMG_1347 2 1.svg";
+import image1 from "../assets/images/50F8B581-8FD8-463C-8C70-0AB5D830AAA4 1.svg";
+import image2 from "../assets/images/Снимок экрана 2026—05—01 в 20.02.53 1.svg";
+import image3 from "../assets/images//IMG_6443 1.svg";
 import styles from "../styles/MainSection.module.css";
 
 const MainSection = () => {
@@ -7,9 +9,18 @@ const MainSection = () => {
     <main className={styles.mainSection}>
       <div className={styles.container}>
         <div className={styles.imageContent}>
-          <div className={styles.imageShadow}></div>
           <div className={styles.image}>
-            <img src={image} alt="image content" />
+            <div className={styles.topRow}>
+              <div className={styles.imageCard}>
+                <img src={image1} alt="Portrait of Monika" />
+              </div>
+              <div className={`${styles.imageCard} ${styles.imageCardShifted}`}>
+                <img src={image2} alt="Portrait of Vars" />
+              </div>
+            </div>
+            <div className={`${styles.imageCard} ${styles.bottomCard}`}>
+              <img src={image3} alt="Portrait of Davit" />
+            </div>
           </div>
         </div>
         <div className={styles.textContent}>
