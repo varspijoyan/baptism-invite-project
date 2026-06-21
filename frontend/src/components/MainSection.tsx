b887af0fe92c581510cf30be8ec44fcb178e2e4f
varspijoyan <varsikpijoyan2003@gmail.com>
2026-06-21 15:09:30 +0400

    core: change image

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -1,7 +1,5 @@
 import crossImg from "../assets/images/Cross.svg";
-import image1 from "../assets/images/50F8B581-8FD8-463C-8C70-0AB5D830AAA4 1.svg";
-import image2 from "../assets/images/IMG_6446 2 1.svg";
-import image3 from "../assets/images/IMG_6443 1.svg";
+import familyPortrait from "../assets/images/family-portrait.png";
 import styles from "../styles/MainSection.module.css";
 
 const MainSection = () => {
@@ -9,18 +7,8 @@ const MainSection = () => {
     <main className={styles.mainSection}>
       <div className={styles.container}>
         <div className={styles.imageContent}>
-          <div className={styles.image}>
-            <div className={styles.topRow}>
-              <div className={styles.imageCard}>
-                <img src={image1} alt="Portrait" />
-              </div>
-              <div className={`${styles.imageCard} ${styles.imageCardShifted}`}>
-                <img src={image2} alt="Portrait" />
-              </div>
-            </div>
-            <div className={`${styles.imageCard} ${styles.bottomCard}`}>
-              <img src={image3} alt="Portrait" />
-            </div>
+          <div className={styles.imageCard}>
+            <img src={familyPortrait} alt="Family portrait" />
           </div>
         </div>
         <div className={styles.textContent}>
